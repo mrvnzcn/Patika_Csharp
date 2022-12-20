2e749507d591457e8a4d290dfc73241a25db49da
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2022-12-20 23:00:22 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Adin:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadin:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Neler Oluyor?" + name + " " + surname);
+        }
+    }
+}
