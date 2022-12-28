32f7c1bbd3d9757b9d496974a6d88e8f25cbd023
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2022-12-28 20:00:28 +0300

    date time ile if else

---
+++ if-else/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >=6 && time < 11)
+                Console.WriteLine("Günaydin");
+            else if ( time <= 18)
+                Console.WriteLine("İyi Günler");
+            else
+                Console.WriteLine("İyi Geceler");
+
+            string sonuc = time <= 18 ? "İyi Günler" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+
+            sonuc = time>=6 && time <11 ? "Gunaydin" : time <= 18 ? "İyi günlers" : "iyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
