42a16068f8a58ac35895a5b1b06cde1ab6b3ae26
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-01-02 20:23:51 +0300

    while ve foreach örnekleri

---
+++ whileforeach/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace whileforeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //while ile 1'den başlayarak console'dan girilen sayıya kadar ortalama yaz, bastır
+            Console.Write("Sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi) 
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            //a'dan z'ye tüm harfleri bas
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+                Console.Write(karakter);
+                karakter++;
+            }
+
+            Console.Write("***** foreach *****");
+            string[] arabalar = {"bmw", "skoada", "toyota", "nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
