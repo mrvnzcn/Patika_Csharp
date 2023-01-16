f71c8796f49e33841b1127c3bbda2a5e011bb426
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-01-17 01:01:20 +0300

    algoritma soruları örnek 1

---
+++ AlgoritmaSorulari-Odev1-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Odev1
+{
+   class Program 
+   {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pozitif bir tam sayı giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+         int[] numbers = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Dizi elemanlarını giriniz: "+(i+1)+". eleman: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Çift sayılar: ");
+        for (int i=0; i < n; i++)
+        {
+            if(numbers[i]%2 == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+   }
+    
+}
