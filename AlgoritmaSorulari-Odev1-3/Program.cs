83d0d2ece030fe67222938d8090dffebfd0619e1
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-01-17 01:48:34 +0300

    algoritma soruları örnek 3

---
+++ AlgoritmaSorulari-Odev1-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Odev1
+{
+   class Program 
+   {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pozitif bir tam sayı giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        
+         string[] words = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Dizi kelimelerini giriniz: "+(i+1)+". kelime: ");
+            words[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Sondan başa kelimeler: ");
+        for (int i=n-1; i >= 0; i--)
+        {
+               
+        Console.WriteLine(words[i]);
+            
+        }
+    }
+   }
+    
+}
