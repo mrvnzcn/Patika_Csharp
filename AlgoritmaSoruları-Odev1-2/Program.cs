facf6e40c0cf245ba9ea9fe0bb9040a187f94bcc
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-01-17 01:19:46 +0300

    algoritma soruları örnek 2

---
+++ AlgoritmaSoruları-Odev1-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Odev1
+{
+   class Program 
+   {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pozitif bir tam sayı giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Pozitif bir tam sayı giriniz: ");
+        int m = int.Parse(Console.ReadLine());
+         int[] numbers = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Dizi elemanlarını giriniz: "+(i+1)+". eleman: ");
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(m+" sayısına tam bölünen sayılar: ");
+        for (int i=0; i < n; i++)
+        {
+            if(numbers[i]%m == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+   }
+    
+}
