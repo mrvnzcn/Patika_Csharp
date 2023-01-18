813e3dcb5e9cea0981c4d86fa1dee647bf27c68d
İsmail Mervan Özcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-01-19 02:43:20 +0300

    konsol uygulaması örnek

---
+++ AlgoritmaSorulari-Odev1-4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            
+            Console.WriteLine("Bir cümle yazınız: ");
+            string cumle = Console.ReadLine();
+
+            string[] kelimeler = cumle.Split(' ');
+            
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                Console.WriteLine("Kullanılan kelimeler: "+kelimeler[i]);
+            }
+            
+            int kelimeSayisi = kelimeler.Length;
+            Console.WriteLine("Kelime Sayısı: " +kelimeSayisi);
+
+            string forharfSayisiCumle = cumle.Replace(" ", "");
+            int harfSayisi = forharfSayisiCumle.Length;
+
+            Console.WriteLine("Harf Sayısı: " +harfSayisi);
+        }
+    }
+}
