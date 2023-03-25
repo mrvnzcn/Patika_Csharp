48eec41c19fef620ecc00a60d11207970fde3966
Ismail Mervan Ozcan | SmartMessage <mervan.ozcan@smartmessage.com>
2023-03-25 18:21:58 +0300

    base ve this key'leri eklendi

---
+++ yapici-metot-kalitim-ornek/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Memeli
+{
+    protected double Boy;
+    protected double Agirlik;
+
+    public Memeli(double boy, double agirlik)
+    {
+        this.Boy = boy;
+        this.Agirlik = agirlik;
+        Console.WriteLine("Memeli hayvani olusturuldu. (yapici/kurucu metot)");
+    }
+
+    public void OzellikGoster()
+    {
+        Console.WriteLine("Boy: "+Boy);
+        Console.WriteLine("Agirlik: "+Agirlik);
+    }
+}
+
+class Kedi : Memeli
+{
+    public string Turu;
+    public Kedi(string turu, int boy, int agirlik):base(boy,agirlik)
+    {
+        this.Turu = turu;
+        Console.WriteLine("Kedi olusturuldu. (yapici/kurucu metot)");
+    }
+
+    public void TuruGoster()
+    {
+        Console.WriteLine(Turu + " Kedisi");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kedi kedi1 = new Kedi("van", 5, 10);
+        kedi1.OzellikGoster();
+        kedi1.TuruGoster();
+        Console.WriteLine("**********");
+        Memeli memeli1 = new Memeli(6,11);
+        memeli1.OzellikGoster();
+    }
+}
